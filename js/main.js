7cd6880760bf0104543506b7cdb4deab3b26eb80
Brendan Quinn <brendanquinn@Brendans-MacBook-Air.local>
2014-09-05 11:01:39 -0400

    no idea

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
   $(".owl-carousel").owlCarousel();
+
+  $('.owl-carousel').owlCarousel({
+        items:10,
+        loop:true,
+        center:true,
+        margin:10,
+        URLhashListener:true,
+        autoplayHoverPause:true,
+        startPosition: 'URLHash'
+    });
+
 });
